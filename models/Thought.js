6f5d2f6638a2cb02c378a3f2c3971cdfc074a5a1
Cody <110630101+CodySaal@users.noreply.github.com>
2023-01-15 13:52:15 -0600

    Thought model created

--- models/Thought.js
+++ models/Thought.js
@@ -1,8 +1,34 @@
 const mongoose = require("mongoose");
+const reactionSchema = require("./Reactions")
+const formatDate = require("../utils/format")
 
 const ThoughtSchema = new mongoose.Schema({
-   
+   thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+   },
+   createdAt: {
+    type: Date,
+    default: Date.now,
+    get: date => formatDate(date),
+   },
+   username: {
+    type: String,
+    required: true,
+   },
+   reactions: [reactionSchema],
+}, {
+    toJSON: {
+        getters: true,
+        virutals: true
+    }
 });
+
+ThoughtSchema.virtual("reactionCount").get(function() {
+    return this.reactions.length
+})
 
 const Thought = mongoose.model("Thought", ThoughtSchema)
 
