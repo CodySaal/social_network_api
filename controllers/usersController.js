ff95eb2fa7d94199c5f102c6c0d5caddd47e25f0
Cody <110630101+CodySaal@users.noreply.github.com>
2023-01-17 12:18:43 -0600

    cleaned up code for deleting thoughts of a deleted user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -36,9 +36,7 @@ module.exports = {
     deleteUser: async function (req, res) {
         try {
             const user = await User.findById(req.params.id)
-            const findUserThoughts = await Thought.deleteMany({
-                _id: { $in: user.thoughts}
-            })
+            Thought.deleteMany({_id: { $in: user.thoughts}})
             const deletedUser = await User.findByIdAndDelete(req.params.id)
             res.json(deletedUser)
         } catch(err) {
